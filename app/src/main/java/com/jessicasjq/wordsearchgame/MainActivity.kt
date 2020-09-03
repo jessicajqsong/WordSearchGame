18f3a2b2b17a744d33241e55401d40348e9b15ab
jessicajqsong <jiaqi.jessica.song@gmail.com>
2020-09-03 10:09:15 -0400

    Updated code comments

--- app/src/main/java/com/jessicasjq/wordsearchgame/MainActivity.kt
+++ app/src/main/java/com/jessicasjq/wordsearchgame/MainActivity.kt
@@ -3,7 +3,6 @@ package com.jessicasjq.wordsearchgame
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.LinearLayout
@@ -11,25 +10,25 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.blue
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.schedule
 
 
 class MainActivity : AppCompatActivity(), View.OnTouchListener {
+    // Initialize the timer
     var totalTimeSpent : Long = 0
     private var startTime : Long = 0
 
+    // Setting up variables needed
     private var xInitial = -1f
     private var yInitial = -1f
-
     private var xDiff = -1f
     private var yDiff = -1f
-
     private var prevXDiff = -1f
     private var prevYDiff = -1f
 
+    // Whether the user is swiping vertically or horizontally
     enum class SwipeState { Undefined, Vertical, Horizontal }
     private var swipeState = SwipeState.Undefined
 
@@ -39,9 +38,14 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startTime = System.currentTimeMillis()  //Timer starts
+        //Timer starts
+        startTime = System.currentTimeMillis()
 
+        // Hide the congratulation message
         congrats_layout.visibility = View.GONE
+        celebrate.visibility = View.GONE
+
+        // Set the width of each grid cell (for a 10*10 grid only)
         cellWidth = resources.displayMetrics.widthPixels/10
 
         for (i in 0 until numWords){
@@ -56,14 +60,16 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
             }
         }
 
-        // Adjusting height fo the grid
+        // Adjust height of the grid
         val params = words_grid.layoutParams as ConstraintLayout.LayoutParams
         params.height = resources.displayMetrics.widthPixels
         words_grid.layoutParams = params
 
+        // Fill the rest of the grid with random letters
         generateRandomLetters()
     }
 
+    // Each time the user swipes the word, a random colour will be used for the stroke
     private var selectedColour = R.drawable.selected_cell_background_blue
 
     override fun onTouch(v: View, event: MotionEvent): Boolean{
@@ -71,6 +77,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 // User started selecting cells
+                // We now select a random colour to colour the cells they touched
                 val random = Random()
                 val randomColour = random.nextInt(5)
                 if(randomColour == 0){
@@ -98,7 +105,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
             }
 
             MotionEvent.ACTION_MOVE -> {
-                // User's still selecting cells
+                // User is still selecting cells
                 if (xInitial != -1f && yInitial != -1f) {
 
                     val tag = v.tag.toString()
@@ -158,7 +165,8 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
             }
 
             MotionEvent.ACTION_UP -> {
-                // User's done selecting cells
+                // User is done selecting cells
+                // We check if the word they touched is valid
                 val tag = v.tag.toString()
                 val tagInt = tag.toInt()
                 var finalTag = tag
@@ -190,6 +198,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         return true
     }
 
+    // If a valid word is swiped over, the word hint will be crossed out and be highlighted green
     private fun crossOutWords(wordObj : Word){
         if (wordObj.length == 4 && wordObj.content.trim() == "JAVA"){
             javaTxt.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
@@ -212,6 +221,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         }
     }
 
+    // Redo all the crossing out and highlighting
     private fun redoAllCrossedOutWords(){
         javaTxt.paintFlags = 0
         swiftTxt.paintFlags = 0
@@ -227,9 +237,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         objectivecTxt.setTextColor(Color.BLACK)
     }
 
-    /**
-     * Determines if the selected range is a one of the hidden words
-     */
+    // Checks whether the word that user found is a one of the target words
     private fun checkIfRangeIsValid(initTag: String, endTag: String){
         var found = false
         for(wordObj in wordArray){
@@ -238,9 +246,10 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
                     endTag.toInt(),
                     swipeState == SwipeState.Horizontal
                 )){
-                // we got a match
+                // It is a match with one of the target words
                 if(wordObj.found){
-                    // the word has been already found
+                    // But the word has been already found
+                    // So it cannot be found again
                     xInitial = -1f
                     yInitial = -1f
                     xDiff = -1f
@@ -251,6 +260,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
                     return
                 }
 
+                // Otherwise the user have found a valid word
                 markCellsAsFound(
                     initTag.toInt(),
                     endTag.toInt(),
@@ -258,11 +268,12 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
                 )
                 wordObj.found = true
                 found = true
-                crossOutWords(wordObj)
+                crossOutWords(wordObj)  // We cross out the hint for this word
                 break
             }
         }
 
+        // If the user found a valid word, display a happy cat
         if (found){
             greenCheck.visibility = View.VISIBLE
             happyCat.visibility = View.VISIBLE
@@ -285,8 +296,9 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
                     break
                 }
             }
+            // If all valid words have been found by the user
+            // Display the congratulation message and the total time spent to finish the game
             if (showCongrats){
-                // Display the congratulation message and the total time spent to finish the game
                 totalTimeSpent = System.currentTimeMillis() - startTime
                 if (totalTimeSpent/1000 >= 60){
                     var minutes = totalTimeSpent/1000/60
@@ -306,11 +318,15 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
                 }
                 happyCat.visibility = View.GONE
                 congrats_layout.visibility = View.VISIBLE
+
+                // Display an even cuter cat
                 winningCat.visibility = View.VISIBLE
                 celebrate.visibility = View.VISIBLE
             }
 
         } else {
+            // Otherwise, the user found a word that is invalid
+            // Display a sad cat
             redX.visibility = View.VISIBLE
             sadCat.visibility = View.VISIBLE
             Timer("delay", false).schedule(1200) {
@@ -322,7 +338,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
             unselectCellRange(initTag.toInt(), endTag.toInt(), swipeState == SwipeState.Horizontal)
         }
 
-        // resetting values
+        // Resetting values
         xInitial = -1f
         yInitial = -1f
         xDiff = -1f
@@ -330,9 +346,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         swipeState = SwipeState.Undefined
     }
 
-    /**
-     * Unselects a range of cells
-     */
+    // Unselect a range of cells
     private fun unselectCellRange(initTag: Int, endTag: Int, isHorizontal: Boolean){
         var start = initTag
         var end = endTag
@@ -351,19 +365,14 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         }
     }
 
-    /**
-     * Selects a cell by tag
-     */
+    // Select a cell by tag
     private fun selectSingleCell(tag: String){
         val childCount = words_grid.childCount
         for (i in 0 until childCount){
             val linearLayout: LinearLayout = words_grid.getChildAt(i) as LinearLayout
             for (t in 0 until linearLayout.childCount){
                 if(linearLayout.getChildAt(t).tag == tag){
-                    linearLayout.getChildAt(t).background = ContextCompat.getDrawable(
-                        this,
-                        selectedColour
-                    )
+                    linearLayout.getChildAt(t).background = ContextCompat.getDrawable(this, selectedColour)
                     return
                 }
             }
@@ -371,9 +380,6 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
     }
 
 
-    /**
-     * Selects a cell by tag
-     */
     private fun unselectSingleCell(tag: String){
         var tagInt = tag.toInt()
         val childCount = words_grid.childCount
@@ -382,10 +388,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
             for (t in 0 until linearLayout.childCount){
                 if(linearLayout.getChildAt(t).tag == tag){
                     if(!foundWordsFlags[tagInt / 10][tagInt % 10]){
-                        linearLayout.getChildAt(t).background = ContextCompat.getDrawable(
-                            this,
-                            R.drawable.unselected_cell_background
-                        )
+                        linearLayout.getChildAt(t).background = ContextCompat.getDrawable(this, R.drawable.unselected_cell_background)
                     }
                     return
                 }
@@ -393,9 +396,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         }
     }
 
-    /**
-     * Flags the found cells so that the don't get unselected until a new game is started
-     */
+    // Flag the found cells so that the don't get unselected until a new game is started
     private fun markCellsAsFound(initTag: Int, endTag: Int, isHorizontal: Boolean){
         var start = initTag
         var end = endTag
@@ -414,9 +415,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         }
     }
 
-    /**
-     * Clears the previously selected cells.
-     */
+    // Clear all previously selected cells
     private fun unselectAllCells(){
         val childCount = words_grid.childCount
         for (i in 0 until childCount){
@@ -430,9 +429,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         }
     }
 
-    /**
-     * Randomly re-arranges the letters in the grid
-     */
+    // Randomly re-arranges the letters in the grid
     private fun generateRandomLetters(){
         gridFlags = Array(gridSize) { BooleanArray(gridSize) { false } }
         foundWordsFlags = Array(gridSize) { BooleanArray(gridSize) { false } }
@@ -454,37 +451,38 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
                 if(words[w].length < gridSize){
                     r = rnd.nextInt(gridSize - (words[w].length))
                 } else if (words[w].length > gridSize){
-                    // maybe throw exception ?
+                    // Not happening since we are only testing with the 6 given words
+                    // that have a max of length 10
                     break
                 }
 
-                // checking all rows or columns depending on the toggle value
+                // Check all rows or columns depending on the toggle value
                 var start = rnd.nextInt(gridSize - 1)
 
                 for (n in 0 until gridSize){
                     var _n = (n + start) % gridSize
-                    // checking if the row or column is empty enough to place the word
+                    // Check if the row or column is empty enough to place the word
                     for (i in r until r + words[w].length ) {
                         if(toggle){
-                            // looking along the row
+                            // Look along the row
                             if(gridFlags[_n][i] && gridLetters[_n][i] != words[w][i - r].toString()) {
                                 break
                             } else if (i == r + words[w].length - 1) {
-                                // we've reached the end
+                                // We've reached the end
                                 found = true
                             }
                         } else {
-                            // looking at along the column
+                            // Look along the column
                             if(gridFlags[i][_n]&& gridLetters[i][_n] != words[w][i - r].toString()) {
                                 break
                             } else if (i == r + words[w].length - 1) {
-                                // we've reached the end
+                                // We've reached the end
                                 found = true
                             }
                         }
                     }
                     if(found) {
-                        // Registering location in Word object
+                        // Register the valid word location in Word object
                         if(toggle){
                             wordArray[w].setLoc(_n * 10 + r, toggle)
                         } else {
@@ -493,11 +491,11 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
 
                         for (i in r until r + words[w].length ) {
                             if(toggle){
-                                // filling along the row
+                                // Filling along the row
                                 gridLetters[_n][i] = words[w][i - r].toString()
                                 gridFlags[_n][i] = true
                             } else {
-                                // filling at along the column
+                                // Filling along the column
                                 gridLetters[i][_n] = words[w][i - r].toString()
                                 gridFlags[i][_n] = true
                             }
@@ -509,7 +507,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
             }
         }
 
-        // displaying letters
+        // Display the letters
         val childCount = words_grid.childCount
         for (i in 0 until childCount){
             val linearLayout: LinearLayout = words_grid.getChildAt(i) as LinearLayout
@@ -519,6 +517,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
         }
     }
 
+    // Re-start the game when the user clicks on the new game button
     fun reStartGame(view: View){
         congrats_layout.visibility = View.GONE
         winningCat.visibility = View.GONE
@@ -536,13 +535,15 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener {
     companion object {
         const val vocabulary = "ABCDEFGHIJKLMOPQRSTUVWSYZ"
         const val numWords = 6
-        const val gridSize = 10
+        const val gridSize = 10 // 10*10 grid
 
         var gridLetters = Array(gridSize) { Array<String>(gridSize) { "A" } }
         var gridFlags = Array(gridSize) { BooleanArray(gridSize) { false } }
         var foundWordsFlags = Array(gridSize) { BooleanArray(gridSize) { false } }
 
         val wordArray = Array<Word>(numWords) { Word("")}
+
+        // The valid words that the user will need to locate
         val words = arrayOf("OBJECTIVEC", "VARIABLE", "MOBILE", "KOTLIN", "SWIFT", "JAVA")
     }
 }
